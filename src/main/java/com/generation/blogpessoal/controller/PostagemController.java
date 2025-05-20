db1a18bde8f2472e9a4255e7306b571252c53395
Pedro Coelho <phcoelho2004@gmail.com>
2025-05-20 17:43:13 -0300

    :sparkles: Criação do Tema Repository e Controller e Relacionamentos

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -30,6 +31,9 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -51,7 +55,11 @@ public class PostagemController {
 	
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		
+		if(temaRepository.existsById(postagem.getTema().getId())) {				
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O tema não existe!", null);
 	}
 	
 	@PutMapping
@@ -60,10 +68,15 @@ public class PostagemController {
 		if(postagem.getId() == null)
 			return ResponseEntity.badRequest().build();
 		
-		if(postagemRepository.existsById(postagem.getId()))			
-			return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
-		
+		if(postagemRepository.existsById(postagem.getId()))	{		
+			if(temaRepository.existsById(postagem.getTema().getId())) {
+			
+				return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+			}
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O tema não existe!", null);
+		}	
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
 	}
 	
 	@DeleteMapping("/{id}")
---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,61 @@
+package com.generation.blogpessoal.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_temas") // CREATE TABLE tb_temas();
+public class Tema {
+
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Long id;
+	
+	@Column(length = 255)
+	@NotBlank(message = "O Atributo Descrição é obrigatório e não pode ser vazio")
+	private String descricao;
+	
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+	
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+	
+	
+}
